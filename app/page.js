c2cdba632be5c414bf53c949d656f75016bd97d2
RobertoMCov <rmiranda@covalu.com>
2024-12-23 10:43:19 -0600

    updatednavbar

--- app/page.js
+++ app/page.js
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-4 flex flex-col items-center justify-center gap-2">
-      <Image src="/mhcorp/logo.png" alt="Logo de la corp :v" width={320} height={320} />
-      <h1 className="text-4xl font-bold">Vistiendo el mañana, hoy :v</h1>
+    <div>
+      <div className="bg-sky-900 height-4 w-full">
+        <p className="text-white md:hidden">A la bestia el menu para fones :v</p>
+        <p className="text-white hidden md:block">A la bestia el menu para compus :v</p>
+      </div>
+      <div className="mt-4 flex flex-col items-center justify-center gap-2">
+        <Image src="/mhcorp/logo.png" alt="Logo de la corp :v" width={320} height={320} />
+        <h1 className="text-4xl font-bold">Vistiendo el mañana, hoy :v</h1>
+      </div>
     </div>
   );
 }
